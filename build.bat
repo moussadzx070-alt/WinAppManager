34e8f46cabb727b89e7d4d17c574205f1258e995
moussadzx070-alt <moussadzx070@gmail.com>
2026-06-26 20:03:51 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
-echo Building WinAppManager EXE...
+echo Building DZ Manager z EXE...
 pip install -r requirements.txt
-pyinstaller --noconfirm --onedir --windowed --icon "resources/icon.ico" --name "WinAppManager" --add-data "resources;resources/" "main.py"
+pyinstaller --noconfirm --onedir --windowed --icon "resources/icon.ico" --name "DZ_Manager_z" --add-data "resources;resources/" "main.py"
 echo Build Complete! Check the 'dist' folder.
 pause
